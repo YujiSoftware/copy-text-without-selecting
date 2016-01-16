1b82ff0251af7a7089f10a7a597a3a9da38132ff
YujiSoftware <yuji.software+github@gmail.com>
2016-01-16 20:26:06 +0900

    Improve block level element text

--- data/menu.js
+++ data/menu.js
@@ -11,47 +11,32 @@ function getText(node, lineSeparator){
   while(node != null){
     if(node.nodeType == Node.TEXT_NODE){
       text += node.nodeValue.trim();
-    }
-
-    if(node.nodeName != "SCRIPT" && isVisible(node)){
-      var childText = ""; 
-      if(node.firstChild != null){
-        if(node.parentNode.nodeName == "SELECT" && node.nodeName == "OPTION"){
-          // Get selected option text only
-          if(node.parentNode[node.parentNode.selectedIndex] == node){
+    }else if(node.nodeType == Node.ELEMENT_NODE){
+      if(node.nodeName != "SCRIPT"){
+        var childText = ""; 
+        if(node.firstChild != null){
+          if(node.parentNode.nodeName == "SELECT" && node.nodeName == "OPTION"){
+            // Get selected option text only
+            if(node.parentNode[node.parentNode.selectedIndex] == node){
+              childText = getText(node.firstChild, lineSeparator);
+            }
+          }else{      
             childText = getText(node.firstChild, lineSeparator);
           }
-        }else{      
-          childText = getText(node.firstChild, lineSeparator);
+        }
+      
+        if(childText != ""){
+          text += childText;
         }
       }
-      
-      if(childText != ""){
-        text += childText;
-      }
-    }
     
-    if(node.nodeName == "BR" || node.nodeName == "P" || node.nodeName == "LI"){
-      text += lineSeparator;
+      if(node.nodeName == "BR" || window.getComputedStyle(node, null).display == "block"){
+        text += lineSeparator;
+      }
     }
 
     node = node.nextSibling;
   }
 
   return text;
-}
-
-function isVisible(node){
-  switch(node.nodeType){
-    case Node.ELEMENT_NODE:
-      return node.offsetWidth > 0 || node.offsetHeight > 0 || node.getClientRects().length > 0
-    case Node.PROCESSING_INSTRUCTION_NODE:
-    case Node.COMMENT_NODE:
-    case Node.DOCUMENT_NODE:
-    case Node.DOCUMENT_TYPE_NODE:
-    case Node.DOCUMENT_FRAGMENT_NODE:
-      return false;
-    default:
-      return true;
-  }
 }
