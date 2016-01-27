31c79a617db0860f5ed892e6131bbcaaf5aa222b
YujiSoftware <yuji.software+github@gmail.com>
2016-01-27 23:24:55 +0900

    Separated code (for unit test)

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ cm.Item({
         cm.PredicateContext(function(context) {
             return context.selectionText == null && !context.isEditable;
         }),
-    contentScriptFile: [ self.data.url("jquery-2.2.0.min.js"), self.data.url("menu.js") ],
+    contentScriptFile: [ self.data.url("jquery-2.2.0.min.js"), self.data.url("menu.js"), self.data.url("content.js") ],
     data: runtime.OS == "WINNT" ? "\r\n" : "\n",
     onMessage: function(text){
         clipboard.set(text);
