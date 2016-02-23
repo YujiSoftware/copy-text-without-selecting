af83f66e7e577c6af27dbc6c0b7480787b1e5208
YujiSoftware <yuji.software+github@gmail.com>
2016-02-24 01:37:26 +0900

    Modify copy range
    Click on text, copy the node's text. But click on outside text, copy
    parentNode's text.

--- data/content.js
+++ data/content.js
@@ -32,3 +32,38 @@ function getText(node, lineSeparator){
 
     return text.trim();
 }
+
+function isInsideText(clickedNode, clickedEvent){
+    var range = document.createRange();
+
+    // 要素内のTextNodeについて繰り返し処理を行う
+    var childNodes = clickedNode.childNodes;
+    for (var i = 0; i < childNodes.length; i++) {
+        var n = childNodes[i];
+        if (n.nodeType == Node.TEXT_NODE) {
+            // TextNodeのすべての文字を表す範囲を指定する
+            var text = n.textContent;
+
+            range.setStart(n, 0);
+            range.setEnd(n, text.length);
+
+            // TextNode内の各行の絶対座標を取得する。
+            var rects = range.getClientRects();
+            for (var j = 0; j < rects.length; j++) {
+                var rect = rects[j];
+
+                // 各行について座標と範囲を判定する
+                if (rect.left <= clickedEvent.clientX && clickedEvent.clientX <= rect.right
+                    && rect.top <= clickedEvent.clientY && clickedEvent.clientY <= rect.bottom) {
+                    return true;
+                }
+            }
+        } else if (n.nodeType == Node.DOCUMENT_NODE || n.nodeType == Node.ELEMENT_NODE){
+            if(isInsideText(n, clickedEvent)){
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
--- data/menu.js
+++ data/menu.js
@@ -1,9 +1,20 @@
+var clickedEvent = null;
+
+document.addEventListener("mousedown", function(event){
+    //right click
+    if(event.button == 2) { 
+        clickedEvent = event;
+    }
+}, true);
+
 self.on('click', function(node, data){
-    var text = getText(node.firstChild, data);
+    var targetNode = isInsideText(node, clickedEvent) ? node : node.parentNode;
+    
+    var text = getText(targetNode.firstChild, data);
     if(text.length > 0){
         self.postMessage(text.trim());
         
-        var rect = node.getBoundingClientRect();
+        var rect = targetNode.getBoundingClientRect();
         var frame = document.createElement("div");
         frame.style.position = "absolute";
         frame.style.top = (rect.top + window.scrollY) + "px";
