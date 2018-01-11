3e005b70f94dea232feaeda9e4ee3753b0914e2e
YujiSoftware <yuji.software+github@gmail.com>
2018-01-12 00:53:23 +0900

    Refactoring

--- menu.js
+++ menu.js
@@ -32,15 +32,18 @@
         
         var rect = clickedElement.getBoundingClientRect();
         var frame = document.createElement("div");
-        frame.style.position = "absolute";
-        frame.style.top = (rect.top + window.scrollY) + "px";
-        frame.style.left = (rect.left + window.scrollX) + "px";
-        frame.style.width = (rect.width - 4) + "px";
-        frame.style.height = (rect.height - 4) + "px";
-        frame.style.border = "solid 2px gold";
-        frame.style.borderRadius = "5px";
-        frame.style.zIndex = "99999";
-        frame.style.pointerEvents = "none";
+        Object.assign(frame.style, {
+            position: "absolute",
+            top: (rect.top + window.scrollY) + "px",
+            left: (rect.left + window.scrollX) + "px",
+            width: (rect.width - 4) + "px",
+            height: (rect.height - 4) + "px",
+            border: "solid 2px gold",
+            borderRadius: "5px",
+            zIndex: "99999",
+            pointerEvents: "none"
+        }); 
+
         document.body.appendChild(frame);
         
         $(frame).fadeIn(300, "swing").delay(500).fadeOut(500, "swing");
